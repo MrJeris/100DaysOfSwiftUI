2d4ac5ab74e50085407431451c5310eb10d0689b
MrJeris <mrjeris@yandex.ru>
2023-05-14 20:52:36 +0300

    Test commit

--- 100DaysOfSwiftUI/Project_12_CoreDataProject/FilteredList.swift
+++ 100DaysOfSwiftUI/Project_12_CoreDataProject/FilteredList.swift
@@ -1,25 +1,38 @@
 //
-//  FilteredList.swift
+//  Filtered.swift
 //  100DaysOfSwiftUI
 //
-//  Created by MrJeris on 09.05.2023.
+//  Created by MrJeris on 14.05.2023.
 //
 
 import CoreData
 import SwiftUI
 
+enum FilterType: String {
+    case beginsWith = "BEGINSWITH"
+    case contains = "CONTAINS[c]"
+}
+
 struct FilteredList<T: NSManagedObject, Content: View>: View {
     @FetchRequest var fetchRequest: FetchedResults<T>
+
+    // this is our content closure; we'll call this once for each item in the list
     let content: (T) -> Content
-    
+
     var body: some View {
-        List(fetchRequest, id: \.self) { item in
-            self.content(item)
+        List(fetchRequest, id: \.self) { singer in
+            self.content(singer)
         }
     }
-    
-    init(filterKey: String, filterValue: String, @ViewBuilder content: @escaping (T) -> Content) {
-        _fetchRequest = FetchRequest<T>(sortDescriptors: [], predicate: NSPredicate(format: "%K BEGINSWITH %@", filterKey, filterValue))
+
+    init(filterType: FilterType = .contains,
+         filterKey: String,
+         filterValue: String,
+         sortDescriptors: [SortDescriptor<T>] = [],
+         @ViewBuilder content: @escaping (T) -> Content
+    ) {
+        _fetchRequest = FetchRequest<T>(sortDescriptors: sortDescriptors, predicate: NSPredicate(format: "%K \(filterType.rawValue) %@", filterKey, filterValue))
         self.content = content
     }
 }
+
--- 100DaysOfSwiftUI/Project_9_Drawing/Constant.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Constant.swift
-//  100DaysOfSwiftUI
-//
-//  Created by MrJeris on 30.04.2023.
-//
-
-import Foundation
